f6e7c95bea4ee60a06612af38e180375b2c7a6a5
jiyuehe <jiyue_he@163.com>
2025-10-08 15:04:02 -0700

    .

--- codes/tools/set_axes_equal.py
+++ codes/tools/set_axes_equal.py
@@ -1,21 +1,11 @@
 import numpy as np
 
-def execute(ax):
+def execute(ax): 
     # make 3D plot axes have equal scale so spheres look like spheres
     x_limits = ax.get_xlim3d()
     y_limits = ax.get_ylim3d()
     z_limits = ax.get_zlim3d()
-
-    x_range = abs(x_limits[1] - x_limits[0])
-    y_range = abs(y_limits[1] - y_limits[0])
-    z_range = abs(z_limits[1] - z_limits[0])
-
-    max_range = max([x_range, y_range, z_range]) / 2.0
-
-    mid_x = np.mean(x_limits)
-    mid_y = np.mean(y_limits)
-    mid_z = np.mean(z_limits)
-
-    ax.set_xlim3d([mid_x - max_range, mid_x + max_range])
-    ax.set_ylim3d([mid_y - max_range, mid_y + max_range])
-    ax.set_zlim3d([mid_z - max_range, mid_z + max_range])
+    max_range = max([abs(x_limits[1] - x_limits[0]), abs(y_limits[1] - y_limits[0]), abs(z_limits[1] - z_limits[0])]) / 2.0
+    ax.set_xlim3d([np.mean(x_limits) - max_range, np.mean(x_limits) + max_range])
+    ax.set_ylim3d([np.mean(y_limits) - max_range, np.mean(y_limits) + max_range])
+    ax.set_zlim3d([np.mean(z_limits) - max_range, np.mean(z_limits) + max_range])
