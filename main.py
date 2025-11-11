85d35d6dffe340ddd8a2232e62893ac9e9408d10
J.H. <jiyue_he@163.com>
2025-11-10 18:21:28 -0800

    .

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This code had been tested in MacOS and Ubuntu.
 # Use Visual Studio Code as the IDE to run this code.
+# Edit the "settings.json" file inside the hidden folder ".vscode" to your own python path.
 
 # Dependencies: 
 # pip install numpy
